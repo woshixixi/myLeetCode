96c61c8b9c0d05bf3e917e5f58756a22506660bd
zcx <zhangchenxi7@outlook.com>
2017-07-24 09:06:33 +0800

    add 二分法

--- 035-SearchInsertPosition.cpp
+++ 035-SearchInsertPosition.cpp
@@ -1,7 +1,8 @@
 /**
  *
  * 最初的想法:直接就是O(n)
- * 还可以用:二分法,O(nlgn)
+ * 还可以用:二分法,O(logn)
+ * 二分法要注意，low<=high 为截止条件，同时，每次low=mid+1，high=mid-1
  *
  */
 
@@ -17,5 +18,25 @@ public:
 
 };
 
-//O(nlgn)
+//O(logn)
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0,high = nums.size() - 1,mid;
+        while(low <= high) {
+            mid =(high + low)/2;
+            if(nums[mid] == target) return mid;
+            if(target > nums[mid]) {
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        if(nums[mid] > target)return mid;
+        else return mid + 1;
+            
+    }
+    
+};
 
